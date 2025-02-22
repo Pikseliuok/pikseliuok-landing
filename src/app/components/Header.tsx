e08ae2172860de6e1929cd080135b4798e3a10a0
Karolis <68164180+Karolinskis@users.noreply.github.com>
2025-02-22 17:46:49 +0200

    Add discord link in header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,27 +1,24 @@
 import React from "react";
+import Image from "next/image";
+import { FaDiscord } from "react-icons/fa";
+import logo from "../../../public/logo.png";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <header className="w-full p-4 bg-background text-foreground shadow-md">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-2xl font-bold">Pikseliuok</h1>
+        <div className="flex items-center space-x-4">
+          <Image src={logo} alt="Pikseliuok Logo" width={40} height={40} />
+          <h1 className="text-2xl font-bold">Pikseliuok</h1>
+        </div>
         <nav>
           <ul className="flex space-x-4">
-            {/* <li>
-              <a href="#" className="text-blue-500 underline">
-                Home
-              </a>
-            </li>
             <li>
-              <a href="#" className="text-blue-500 underline">
-                About
-              </a>
+              <Link href={"https://discord.gg/nwNAWVSuur"}>
+                <FaDiscord size={24} className="text-blue-500 cursor-pointer" />
+              </Link>
             </li>
-            <li>
-              <a href="#" className="text-blue-500 underline">
-                Contact
-              </a>
-            </li> */}
           </ul>
         </nav>
       </div>
