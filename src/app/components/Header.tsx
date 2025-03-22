e20b160efaee82441676a81020455d821633d10d
Karolis <68164180+Karolinskis@users.noreply.github.com>
2025-03-22 12:15:10 +0200

    Add socials

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import { FaDiscord } from "react-icons/fa";
+import { FaInstagram, FaDiscord } from "react-icons/fa";
 import logo from "../../../public/logo.png";
 import Link from "next/link";
 
@@ -13,10 +13,29 @@ const Header = () => {
           <h1 className="text-2xl font-bold">Pikseliuok</h1>
         </div>
         <nav>
-          <ul className="flex space-x-4">
+          <ul className="flex space-x-4 items-center">
             <li>
-              <Link href={"https://discord.gg/nwNAWVSuur"}>
-                <FaDiscord size={24} className="text-blue-500 cursor-pointer" />
+              <Link
+                href={"https://instagram.com/pikseliuok"}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaInstagram
+                  size={24}
+                  className="text-pink-500 hover:text-pink-600 transition-colors cursor-pointer"
+                />
+              </Link>
+            </li>
+            <li>
+              <Link
+                href={"https://discord.gg/nwNAWVSuur"}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaDiscord
+                  size={24}
+                  className="text-blue-500 hover:text-blue-600 transition-colors cursor-pointer"
+                />
               </Link>
             </li>
           </ul>
