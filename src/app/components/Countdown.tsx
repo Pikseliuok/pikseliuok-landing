0c7e2af7776406cb508167f3ea3806478a9a6698
Karolis <68164180+Karolinskis@users.noreply.github.com>
2026-01-25 18:06:05 +0200

    add countdown

---
+++ src/app/components/Countdown.tsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+
+const targetUnixTime = 1774116000;
+
+const Countdown = () => {
+  const [countdown, setCountdown] = useState(
+    targetUnixTime - Math.floor(Date.now() / 1000),
+  );
+
+  useEffect(() => {
+    if (countdown > 0) {
+      const timer = setInterval(() => {
+        setCountdown(targetUnixTime - Math.floor(Date.now() / 1000));
+      }, 1000);
+
+      return () => clearInterval(timer);
+    }
+  }, [countdown]);
+
+  interface TimeParts {
+    days: number;
+    hours: number;
+    minutes: number;
+    seconds: number;
+  }
+
+  const getTimeParts = (totalSeconds: number): TimeParts => {
+    const days = Math.floor(totalSeconds / (24 * 3600));
+    const hours = Math.floor((totalSeconds % (24 * 3600)) / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    const seconds = totalSeconds % 60;
+
+    return { days, hours, minutes, seconds };
+  };
+
+  const { days, hours, minutes, seconds } = getTimeParts(countdown);
+
+  return (
+    <div className="bg-gradient-to-br bg-background p-8 rounded-2xl shadow-lg w-auto">
+      <div className="relative z-10 flex flex-col items-center space-y-4 text-center text-4xl font-bold">
+        {countdown > 0 ? (
+          <div className="flex flex-nowrap justify-center overflow-x-auto px-2">
+            {[
+              { value: days, label: "Dienos" },
+              { value: hours, label: "Valandos" },
+              { value: minutes, label: "Minutės" },
+              { value: seconds, label: "Sekundės" },
+            ].map((unit, index) => (
+              <div
+                key={index}
+                className="flex flex-col items-center justify-center backdrop-blur-lg p-4 rounded-xl shadow-inner m-2"
+              >
+                <span className="text-6xl">
+                  {unit.value.toString().padStart(2, "0")}
+                </span>
+                <span className="mt-2 text-sm tracking-wider uppercase">
+                  {unit.label}
+                </span>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div className="text-3xl">Renginys prasidėjo!</div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Countdown;
