577b7db906849a1430ca5924bf4799c826d118fe
Karolis <68164180+Karolinskis@users.noreply.github.com>
2026-02-28 16:50:00 +0200

    add health check

---
+++ src/app/api/health-check/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+
+export async function HEAD() {
+  try {
+    const response = await fetch("http://localhost:3001", {
+      method: "HEAD",
+      signal: AbortSignal.timeout(3000),
+    });
+
+    if (response.ok) {
+      return new NextResponse(null, { status: 200 });
+    } else {
+      return new NextResponse(null, { status: 503 });
+    }
+  } catch (error) {
+    return new NextResponse(null, { status: 503 });
+  }
+}
+
+export async function GET() {
+  return HEAD();
+}
